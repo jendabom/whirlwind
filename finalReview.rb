53ca0718f5327f22815ab26025cc77ad46d0a89d
Jennifer Blom <jennifer.blom@betterment.com>
2018-03-01 15:23:03 -0500

    refactor final review to use classes

--- finalReview.rb
+++ finalReview.rb
@@ -1,47 +1,50 @@
-def student
-  p "Enter the student's name:"
-  @student_name = gets.chomp
-end
+class Student
 
-@students = []
-
-def how_many_students
-  p "How many Students does your class have?"
-  @class_size = gets.chomp.to_i
-end
-
-def create_students(_student)
-  student_name = @student_name
-end
-
-def enter_class_info
-  how_many_students
-  @class_size.times do
-    @students.push(create_students(student))
+  def get_student
+    p "Enter the student's name:"
+    @student_name = gets.chomp
   end
+
 end
 
-enter_class_info
-
-def make_groups
-  @groups = @students.shuffle.each_slice(2).to_a
-  if @groups.last.length == 1
-    (@groups.first << @groups.last).flatten!
-    @groups.pop(1)
+class Group
+  def how_many_students
+    p "How many Students does your class have?"
+    @class_size = gets.chomp.to_i
   end
-  return @groups
-end
 
-def print_groups(_make_groups)
-  num = 1
-  @groups.each do |group|
-    if group.length == 2
-      p "Group #{num}: #{group[0]}, #{group[1]}"
-    else
-      p "Group #{num}: #{group[0]}, #{group[1]}, and #{group[2]}"
+  def enter_class_info
+    @students = []
+    how_many_students
+    @class_size.times do
+      @students.push(Student.new.get_student)
     end
-    num += 1
+    return @students
+  end
+
+  def make_groups
+    @groups = @students.shuffle.each_slice(2).to_a
+    if @groups.last.length == 1
+      (@groups.first << @groups.last).flatten!
+      @groups.pop(1)
+    end
+    return @groups
+  end
+
+  def print_groups(_make_groups)
+    num = 1
+    @groups.each do |group|
+      if group.length == 2
+        p "Group #{num}: #{group[0]}, #{group[1]}"
+      else
+        p "Group #{num}: #{group[0]}, #{group[1]}, and #{group[2]}"
+      end
+      num += 1
+    end
   end
 end
 
-print_groups(make_groups)
+study_groups = Group.new
+study_groups.enter_class_info
+study_groups.make_groups
+study_groups.print_groups(@groups)
